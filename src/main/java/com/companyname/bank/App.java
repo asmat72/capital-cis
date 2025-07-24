d403255efacef2c4cda1b35bb86c0cec28f98346
asmat72 <145644867+asmat72@users.noreply.github.com>
2025-07-24 15:26:31 +0530

    code change on the project

--- src/main/java/com/companyname/bank/App.java
+++ src/main/java/com/companyname/bank/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "new commit for github webhook");
+        System.out.println( "new code change on the project");
     }
 }
